b23267a9ec29a398047bffd70a019a61a6d03c5d
Emily Blair <emilyblair96@gmail.com>
2021-09-21 14:47:54 -0600

    Update app.test.js file with correct function name

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
 it('Calculate tip test with values of 10 & 15', () => {
   const calculateTip = jest.fn((meal, tip) => Number((meal * (tip/100)).toFixed(2)));
-  expect(calculateTipTest(10,15)).toBe(1.5);
+  expect(calculateTip(10,15)).toBe(1.5);
 })
