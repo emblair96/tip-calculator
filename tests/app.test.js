ccd4da21da1bc91b5510a9c620e03a231bec7235
Emily Blair <emilyblair96@gmail.com>
2021-09-22 11:33:08 -0600

    Add test subfolder for cleaner folder structure

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,14 @@
-const app = require("./app");
+const app = require("../app");
 
-it("Calculate tip test with values of 10 & 15", () => {
+it("Mock calculate tip function & test various values", () => {
   const calculateTip = jest.fn((meal, tip) =>
     Number((meal * (tip / 100)).toFixed(2))
   );
   expect(calculateTip(10, 15)).toBe(1.5);
   expect(calculateTip(22.57, 20)).toBe(4.51);
-  // expect(calculateTip(10, 15)).toBe(1.5);
 });
 
-it("test with mocking app", () => {
+it("mock event listener and check click event happens", () => {
   document.body.innerHTML = '<button id="calculateBtn">Calculate Tip</button>';
 
   let calculateBtn = document.querySelector("#calculateBtn");
